401c0851cb5b333df8f6d7a872c79de009971fc3
lic <2924942127@qq.com>
2026-03-12 13:49:44 +0800

    stack和queue

--- bit/21stack_queue.cpp
+++ bit/21stack_queue.cpp
@@ -79,28 +79,28 @@ private:
 };
 
 
-void test3()
+void test4()
 {
-  // 不能用vector，因为没有pop_front;
-  /*
-   *queue<int, vector<int>> q;  insert或者erase也可以的，效率不高呢的。听劝吧
-   */
+  deque<int> d;
+  d.push_back(1);
+  d.push_back(2);
+  d.push_back(3);
+  d.push_back(4);
+  d.push_back(5);
 
+  d.push_front(9);
+  d.push_front(8);
+  d.push_front(7);
+  d.push_front(6);
+  d.push_front(0);
 
-  queue<int, list<int>> q;
-  q.push(1);
-  q.push(2);
-  q.push(3);
-  q.push(4);
-  q.push(5);
-
-  while(!q.empty())
+  for(auto& e : d )
   {
-    cout<<q.front() << " ";
-    q.pop();
+    cout<< e << " ";
   }
   cout<<endl;
 }
+
 
 int main()
 {
@@ -109,10 +109,26 @@ int main()
 // c++是一个极度关注效率的语言
 // 导致因为一丁点效率变的扭曲
 // 比如：类型萃取
-  test3();
+ /*
+  *test3();
+  */
 
+//deque双关队列 既有list的优点和vector的优点呢
+//operator[]
+//支持push_back,push_front
+//支持pop_back, pop_front
+//deque缺点呢
+  test4(); // 随机访问的效率不如vector
 
+// stack和queue没有涉及到它的随机访问呢。
+// stack和queue只是设计头和尾的操作呢。
+// stack和queue没有迭代器，因为只需要在头部和尾部进行操作呢
 
+//deque
+//头插尾插数据满了直接开辟新的空间,不扩容的
+//中控映射的方式[指针数据的方式]
+//operator[]：计算在那个指针指向的位置呢。
+//迭代器相当的复杂，四个指针，
 
   return 0;
 }
@@ -158,6 +174,29 @@ void test2()
   {
     cout<< st.top() << " ";
     st.pop();
+  }
+  cout<<endl;
+}
+
+void test3()
+{
+  // 不能用vector，因为没有pop_front;
+  /*
+   *queue<int, vector<int>> q;  insert或者erase也可以的，效率不高呢的。听劝吧
+   */
+
+
+  queue<int, list<int>> q;
+  q.push(1);
+  q.push(2);
+  q.push(3);
+  q.push(4);
+  q.push(5);
+
+  while(!q.empty())
+  {
+    cout<<q.front() << " ";
+    q.pop();
   }
   cout<<endl;
 }
---
+++ bit/22stack_queue.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+using namespace std;
+
+template<class T>
+struct Less 
+{
+  bool operator()(const T& x1, const T& x2)
+  {
+    return x1 < x2;
+  }
+};
+
+template<class T>
+struct Greater
+{
+  bool operator()(const T& x1, const T& x2)
+  {
+    return x1 > x2;
+  }
+};
+
+template<class T, class Container = vector<T>, class Compare = less<T>>
+class priority_queue 
+{
+private:
+  void adJustUp(int child)
+  {
+   int parent = (child - 1)/2;
+   Compare com;
+   while(child > 0)
+   {
+     //if(_con[child] > _con[parent])
+     if(com(_con[parent], _con[child]))
+     {
+       swap(_con[parent], _con[child]);
+       child = parent;
+       parent = (child - 1) / 2;
+     }
+     else 
+     {
+       break;
+     }
+   }
+  }
+  void  adJustDown(int root)
+  {
+    Compare com;
+    int parent = root;
+    int child = parent * 2 + 1;
+    while(child < _con.size())
+    {
+      /*
+       *if(child + 1 < _con.size() && _con[child + 1] > _con[child])
+       */
+      if(child + 1 < _con.size() && com(_con[child], _con[child + 1]))
+      {
+        ++child;
+      }
+
+      /*
+       *if(_con[child] > _con[parent])
+       */
+      if(_con[parent], _con[child])
+      {
+        swap(_con[child], _con[parent]);
+        parent = child;
+        child  = parent * 2 + 1;
+      }
+      else 
+      {
+        break;
+      }
+    }
+  }
+public:
+  void push(const T& x) // 向上调整算法
+  {
+    _con.push_back(x);
+    adJustUp(_con.size() - 1);
+  }
+
+  void pop()
+  {
+    swap(_con[0], _con[_con.size() - 1]);
+    _con.pop_back();
+    adJustDown(0);
+  }
+
+  T& top()
+  {
+    return _con[0];
+  }
+
+  size_t size() const 
+  {
+    return _con.size();
+  }
+
+  bool empty() const 
+  {
+    return _con.empty();
+  }
+private:
+  Container _con;
+};
+
+void test1()
+{
+  priority_queue<int> pq;
+  pq.push(1);
+  pq.push(3);
+  pq.push(9);
+  pq.push(5);
+  pq.push(7);
+  while(!pq.empty())
+  {
+    cout<< pq.top() << " ";
+    pq.pop();
+  }
+  cout<<endl;
+}
+
+//处了默认访问 限定符不一样，struct和class在c++中是一样的
+//全部公用struct, 私有公用class
+//仿函数,实际上是重载了(),它的对象可以向函数一样去使用的。
+
+
+// 不要意思c++这里写错了的
+
+int main()
+{
+
+// deque数组指针开始从中间插入数据的
+// 迭代器四个指针
+// 1.operator[] 效率低
+// 2.迭代器遍历相对复杂，效率也有影响的
+
+// vector优点 头插头删效率低，增容代价大
+// list不支持随机访问的。
+// deque的优点
+// T* [N] 数组指针
+
+
+// 优先级队列
+// 优先级高的数据先进行出去的
+// priority_queue
+// 它的适配器就是vector
+
+// 适配出来的容器，一般都不支持迭代器遍历，因为通常包含了一些特殊性质的
+
+// 堆
+// 完全二叉树
+// 需要和下标完全结合在一起的
+//   
+  test1();
+  Less<int> lessfunc;
+  cout<< lessfunc(1, 2) <<endl;
+  cout<< lessfunc.operator()(100, 2) <<endl;
+
+  return 0;
+}
+
+void test()
+{
+// 默认是大堆数据结构实现的
+  // 想实现小的优先级高呢，仿函数登场
+/*
+ * priority_queue<int> pq;
+ * pq.push(11);
+ * pq.push(12);
+ * pq.push(13);
+ * pq.push(14);
+ * pq.push(15);
+ *  
+ * while(!pq.empty())
+ * {
+ *   cout<< pq.top() << " ";
+ *   pq.pop();
+ * }
+ * cout<<endl;
+ *
+ * priority_queue<int, vector<int>, greater<int>> pq2; // 仿函数登场呢
+ * pq2.push(5);
+ * pq2.push(4);
+ * pq2.push(3);
+ * pq2.push(2);
+ * pq2.push(1);
+ *
+ * while(!pq2.empty())
+ * {
+ *   cout<< pq2.top() << " ";
+ *   pq2.pop();
+ * }
+ * cout<<endl;
+ */
+}
